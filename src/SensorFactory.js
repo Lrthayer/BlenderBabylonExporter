33acb5a2fed3325e27b8327d2fbe0e966974074e
Lrthayer <lrthayer@iupui.edu>
2017-04-16 02:04:10 -0400

    fixed sensors
    found out the action manager was being overwriten each time by every new
    sensor because I didn't define a action handler in the main init before
    creating them.

--- src/SensorFactory.js
+++ src/SensorFactory.js
@@ -38,54 +38,17 @@ function SensorFactory() {
             var key = object.key.toString().toLowerCase();
             key = key.replace(/^\s+|\s+$/g,"");
             //console.log(object.key);
-            object.actionManager = new BABYLON.ActionManager(sceneForKey);
-            var test = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) {
-                if (evt.sourceEvent.key == key)
-                {
-                    console.log("wut");
-                    for (i = 0; i < actuators.length; i++) {
-                        actuators[i].say();
-                    }
-                }
-            })
-            object.actionManager.registerAction(test);
-            //Singleton.getInstance().registerAction(test);
-            /*
-			console.log((actuators));
-            if (keysDown[BlenderKeyConversion[object.key]])
-            {
-                console.log("step 1");
-				//get active actuators
-				
-				for (i=0; i < actuators.length; i++)
-				{
-					actuators[i].say();
-				}
-				
-				
 			
-                //go through controllers
-                for (i=0; i < object.controllers.length; i++)
-                {
-                    //if active continue to actuator
-                    if (object.controllers[i].active)
-                    {
-                        //go through actuators
-                        for (j=0; j < object.controllers[i].actuators.length; j++)
-                        {
-                            //if actuator is MOTION apply motion
-                            if (object.controllers[i].actuators[j].type == "MOTION")
-                            {
-                                console.log(babylonObject.name);
-                                console.log();
-                                babylonObject.position.x = babylonObject.position.x + object.controllers[i].actuators[j].offsetLocation[0];
-                                babylonObject.position.y = babylonObject.position.y + object.controllers[i].actuators[j].offsetLocation[1];
-                                babylonObject.position.z = babylonObject.position.x + object.controllers[i].actuators[j].offsetLocation[2];
-                            }
-                        }
-                    }
-                }
-				*/
+			sceneForKey.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) 
+			{
+				if (evt.sourceEvent.key == key) 
+				{
+					for (i=0; i < actuators.length; i++)
+					{
+						actuators[i].say();
+					}
+				}
+			}));
         }
     }
 
